740d2a6e4da6fb93cb4461f3bd9b2978aadf8326
Kévin <kevin.blaise24@gmail.com>
2022-12-12 17:55:31 +0100

    add action btn on List.jsx

--- web-app/src/Pages/Errors/List.jsx
+++ web-app/src/Pages/Errors/List.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Table from '../../Components/Molecules/Table/Table';
 import { BsPencilSquare, BsTrash, BsReceipt } from 'react-icons/bs';
 
@@ -7,8 +8,20 @@ export default function ErrorsList() {
         {project: 'test', date: '12/12/2022', code: 500, status: 'viewed'},
         {project: 'test', date: '12/12/2022', code: 500, status: 'viewed'},
     ];
+    
+    const projects = [...new Set(data.map(item => item.project))];
 
     return (
+        <>
+        <div className="flex justify-between mb-4">
+            <select name="" id="">
+                <option value="All">All</option>
+                {projects.map((item, index) => (
+                    <option value={item} key={index}>{item}</option>
+                ))}
+            </select>
+            <Link to="/projects/add" className="bg-dogger-orange-400 hover:bg-dogger-orange-500 text-white font-bold py-2 px-4 rounded-md">Add project</Link>
+        </div>
         <Table
             tableTitles={['Project', 'Date', 'Code', 'Status']}
             tableKeys={['project', 'date', 'code', 'status']}
@@ -19,5 +32,6 @@ export default function ErrorsList() {
                 { name: 'Delete', link: '/projects/delete', icon: <BsTrash />, hover: 'hover:text-red-500' }
             ]}
         />
+    </>
     );
 };
--- web-app/src/Pages/Projects/List.jsx
+++ web-app/src/Pages/Projects/List.jsx
@@ -1,5 +1,6 @@
-import Table from '../../Components/Molecules/Table/Table';
+import { Link } from "react-router-dom";
 import { BsPencilSquare, BsTrash } from 'react-icons/bs';
+import Table from '../../Components/Molecules/Table/Table';
 
 export default function ProjectsList() {
     const data = [
@@ -9,14 +10,19 @@ export default function ProjectsList() {
     ];
 
     return (
-        <Table
-            tableTitles={['Project', 'Created at']}
-            tableKeys={['name', 'created_at']}
-            data={data}
-            actions={[
-                { name: 'Edit', link: '/projects/edit', icon: <BsPencilSquare /> },
-                { name: 'Delete', link: '/projects/delete', icon: <BsTrash />, hover: 'hover:text-red-500' }
-            ]}
-        />
+        <>
+            <div className="flex justify-end mb-4">
+                <Link to="/projects/add" className="bg-dogger-orange-400 hover:bg-dogger-orange-500 text-white font-bold py-2 px-4 rounded-md">Add project</Link>
+            </div>
+            <Table
+                tableTitles={['Project', 'Created at']}
+                tableKeys={['name', 'created_at']}
+                data={data}
+                actions={[
+                    { name: 'Edit', link: '/projects/edit', icon: <BsPencilSquare /> },
+                    { name: 'Delete', link: '/projects/delete', icon: <BsTrash />, hover: 'hover:text-red-500' }
+                ]}
+            />
+        </>
     );
 };
