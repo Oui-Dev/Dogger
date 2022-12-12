2edf9f2087ebc262672066617963f6a5cc4ed887
Kévin <kevin.blaise24@gmail.com>
2022-12-12 11:49:51 +0100

    add routing

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import './App.scss';
+// Components
 import Navbar from './Components/Atoms/Navbar/Navbar';
 import Footer from './Components/Atoms/Footer';
-import './App.scss';
+// Pages
+import Home from './Pages/Home';
+import ProjectsList from './Pages/Projects/List';
+import ProjectsAdd from './Pages/Projects/Add';
+import ProjectsEdit from './Pages/Projects/Edit';
+import ErrorsList from './Pages/Errors/List';
+import ErrorsDetails from './Pages/Errors/Details';
 
 function App() {
   return (
     <div className="App">
         <section className="min-h-screen">
             <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/projects" element={<ProjectsList />} />
+                <Route path="/projects/add" element={<ProjectsAdd />} />
+                <Route path="/projects/edit" element={<ProjectsEdit />} />
+                <Route path="/errors" element={<ErrorsList />} />
+                <Route path="/errors/details" element={<ErrorsDetails />} />
+            </Routes>
         </section>
         <Footer />
     </div>
