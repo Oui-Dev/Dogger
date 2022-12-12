3ef92240d58945f2ce3ca9711e487dc877526145
Kévin <kevin.blaise24@gmail.com>
2022-12-12 10:59:35 +0100

    add tailwind colors

--- web-app/tailwind.config.js
+++ web-app/tailwind.config.js
@@ -1,8 +1,30 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./src/**/*.{html,js,jsx}"],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                'dogger-orange': {
+                    '50': '#fff6ed',
+                    '100': '#ffead4',
+                    '200': '#ffd1a8',
+                    '300': '#ffb170',
+                    '400': '#ff8437',
+                    '500': '#ff5f0b',
+                    '600': '#f04706',
+                    '700': '#c73207',
+                    '800': '#9e290e',
+                    '900': '#7f250f',
+                },
+                'dogger-gray': '#666666',
+                'dogger-light-gray': '#F5F5F5',
+            }
+        },
+    },
+};
