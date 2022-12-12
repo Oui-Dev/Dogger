70f0e8422bb61d62cd6884035d687b4f4cef106b
Kévin <kevin.blaise24@gmail.com>
2022-12-12 15:23:32 +0100

    use Table components

--- web-app/src/Pages/Errors/List.jsx
+++ web-app/src/Pages/Errors/List.jsx
@@ -1,3 +1,22 @@
-export default function ErrorsList() {
+import Table from '../../Components/Molecules/Table/Table';
+import { BsPencilSquare, BsTrash } from 'react-icons/bs';
 
+export default function ErrorsList() {
+    const data = [
+        {project: 'test', date: '12/12/2022', code: 500, status: 'viewed'},
+        {project: 'test', date: '12/12/2022', code: 500, status: 'viewed'},
+        {project: 'test', date: '12/12/2022', code: 500, status: 'viewed'},
+    ];
+
+    return (
+        <Table
+            tableTitles={['Project', 'Date', 'Code', 'Status']}
+            tableKeys={['project', 'date', 'code', 'status']}
+            data={data}
+            actions={[
+                { name: 'Edit', link: '/projects/edit', icon: <BsPencilSquare />, hover: 'hover:text-dogger-orange-400' },
+                { name: 'Delete', link: '/projects/delete', icon: <BsTrash /> }
+            ]}
+        />
+    );
 };
--- web-app/src/Pages/Projects/List.jsx
+++ web-app/src/Pages/Projects/List.jsx
@@ -2,13 +2,17 @@ import Table from '../../Components/Molecules/Table/Table';
 import { BsPencilSquare, BsTrash } from 'react-icons/bs';
 
 export default function ProjectsList() {
+    const data = [
+        {name: 'test1', created_at: 'test1'},
+        {name: 'test2', created_at: 'test2'},
+        {name: 'test3', created_at: 'test3'}
+    ];
+
     return (
         <Table
             tableTitles={['Project', 'Created at']}
             tableKeys={['name', 'created_at']}
-            data={[
-                {name: 'test', created_at: 'test'}
-            ]}
+            data={data}
             actions={[
                 { name: 'Edit', link: '/projects/edit', icon: <BsPencilSquare />, hover: 'hover:text-dogger-orange-400' },
                 { name: 'Delete', link: '/projects/delete', icon: <BsTrash /> }
