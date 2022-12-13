62c3fe8daecdd8cb74d830cd68046e28d260f4a1
Kévin <kevin.blaise24@gmail.com>
2022-12-13 16:28:57 +0100

    add models

---
+++ api/app/Models/Error.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Error extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'project_id',
+        'code',
+        'message',
+        'path',
+        'line',
+        'status',
+        'timestamp',
+        'viewed_at',
+    ];
+
+    public function project() {
+        return $this->belongsTo(Project::class);
+    }
+}
