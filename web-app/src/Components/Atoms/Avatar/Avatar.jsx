1230d20687e59f4032c8974041cc18302690e305
Oltean Petru <poltean33@gmail.com>
2022-12-15 12:25:43 +0100

    profile form

--- web-app/src/Components/Atoms/Avatar/Avatar.jsx
+++ web-app/src/Components/Atoms/Avatar/Avatar.jsx
@@ -2,7 +2,7 @@ export default function Avatar({...props}){
     return (
         <div className="inline-block h-12 w-12 overflow-hidden rounded-full bg-gray-100">
             {props.initials ? (
-                <div className="flex items-center justify-center bg-orange-400 text-white rounded-full h-12 w-12">
+                <div className="flex items-center justify-center bg-dogger-orange-300 text-white rounded-full h-12 w-12 tracking-widest font-bold">
                     {props.initials}
                 </div>
             ) : (
--- web-app/src/Pages/Profile.jsx
+++ web-app/src/Pages/Profile.jsx
@@ -7,7 +7,80 @@ export default function Profile() {
 				<form action="#" method="POST">
 					<div className="overflow-hidden shadow sm:rounded-md">
 						<div className="bg-white px-4 py-5 sm:p-6">
-                <Avatar  />	
+							<Avatar initials="KB" />
+							<div className="grid grid-cols-6 gap-6">
+								<div className="col-span-6 sm:col-span-3">
+									<label htmlFor="firstName" className="block text-sm font-medium text-gray-700">
+										First name
+									</label>
+									<input
+										type="text"
+										name="firstName"
+										id="firstName"
+										className="mt-1 block w-full rounded-md border-gray-300 shadow-sm sm:text-sm"
+									/>
+								</div>
+
+								<div className="col-span-6 sm:col-span-3">
+									<label htmlFor="lastName" className="block text-sm font-medium text-gray-700">
+										Last name
+									</label>
+									<input
+										type="text"
+										name="lastName"
+										id="lastName"
+										className="mt-1 block w-full rounded-md border-gray-300 shadow-sm sm:text-sm"
+									/>
+								</div>
+
+								<div className="col-span-6">
+									<label htmlFor="email" className="block text-sm font-medium text-gray-700">
+										Email address
+									</label>
+									<input
+										type="text"
+										name="email"
+										id="email"
+										className="mt-1 block w-full rounded-md border-gray-300 shadow-sm sm:text-sm"
+									/>
+								</div>
+
+								<div className="col-span-6 sm:col-span-6 lg:col-span-2">
+									<label htmlFor="password" className="block text-sm font-medium text-gray-700">
+										Current Password
+									</label>
+									<input
+										type="password"
+										name="password"
+										id="password"
+										className="mt-1 block w-full rounded-md border-gray-300 shadow-sm sm:text-sm"
+									/>
+								</div>
+
+								<div className="col-span-6 sm:col-span-3 lg:col-span-2">
+									<label htmlFor="passwordNew" className="block text-sm font-medium text-gray-700">
+										New Password
+									</label>
+									<input
+										type="password"
+										name="passwordNew"
+										id="passwordNew"
+										className="mt-1 block w-full rounded-md border-gray-300 shadow-sm sm:text-sm"
+									/>
+								</div>
+
+								<div className="col-span-6 sm:col-span-3 lg:col-span-2">
+									<label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">
+										Confirm New Password
+									</label>
+									<input
+										type="password"
+										name="confirmPassword"
+										id="confirmPassword"
+										className="mt-1 block w-full rounded-md border-gray-300 shadow-sm sm:text-sm"
+									/>
+								</div>
+							</div>
 						</div>
 						<div className="bg-gray-50 px-4 py-3 flex justify-between sm:px-6">
 							<button
