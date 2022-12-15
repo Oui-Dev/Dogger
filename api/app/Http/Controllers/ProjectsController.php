cbb7a77a544d3b9f3154aa887829ca6ef1587384
Kévin <kevin.blaise24@gmail.com>
2022-12-15 10:04:04 +0100

    add projects endpoint

--- api/app/Http/Controllers/ProjectsController.php
+++ api/app/Http/Controllers/ProjectsController.php
@@ -18,14 +18,54 @@ class ProjectsController extends Controller
     }
 
     public function create() {
-        
+        $currentUser = request()->user();
+
+        $data = request()->validate([
+            'name' => ['required', 'string', 'max:255'],
+        ]);
+
+        Project::create([
+            'user_id' => $currentUser->id,
+            'name' => $data['name'],
+        ]);
+
+        return response()->json([
+            'state' => 'success',
+        ]);
     }
 
-    public function update() {
-        
+    public function update(Project $project) {
+        if($project->user_id !== request()->user()->id) {
+            return response()->json([
+                'state' => 'error',
+                'message' => 'You are not allowed to access to this ressource.',
+            ], 403);
+        }
+
+        $data = request()->validate([
+            'name' => ['required', 'string', 'max:255'],
+        ]);
+
+        $project->name = $data['name'];
+        $project->save();
+
+        return response()->json([
+            'state' => 'success',
+        ]);
     }
 
-    public function delete() {
-        
+    public function delete(Project $project) {
+        if($project->user_id !== request()->user()->id) {
+            return response()->json([
+                'state' => 'error',
+                'message' => 'You are not allowed to access to this ressource.',
+            ], 403);
+        }
+
+        $project->delete();
+
+        return response()->json([
+            'state' => 'success',
+        ]);
     }
 }
