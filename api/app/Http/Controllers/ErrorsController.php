7a6d5524de6b2404659380d572bca06401925a85
Kévin <kevin.blaise24@gmail.com>
2022-12-15 10:04:21 +0100

    [WIP] add errors endpoint

--- api/app/Http/Controllers/ErrorsController.php
+++ api/app/Http/Controllers/ErrorsController.php
@@ -21,11 +21,37 @@ class ErrorsController extends Controller
         
     }
 
-    public function updateStatus() {
-        
+    public function updateStatus(Error $error) {
+        if($error->user_id !== request()->user()->id) {
+            return response()->json([
+                'state' => 'error',
+                'message' => 'You are not allowed to access to this ressource.',
+            ], 403);
+        }
+
+        $data = request()->validate([
+            'status' => ['required', 'integer', 'min:0', 'max:3'],
+        ]);
+
+        $error->status = $data['status'];
+        $error->save();
     }
 
-    public function assignTo() {
-        
+    public function assignTo(Error $error) {
+        if($error->user_id !== request()->user()->id) {
+            return response()->json([
+                'state' => 'error',
+                'message' => 'You are not allowed to access to this ressource.',
+            ], 403);
+        }
+
+        $data = request()->validate([
+            'email' => ['required','email:rfc,dns,spoof','max:255'],
+        ]);
+
+        // TODO : send un email to the user to notify him that he has been assigned to this error
+
+        $error->assigned_to = $data['email'];
+        $error->save();
     }
 }
