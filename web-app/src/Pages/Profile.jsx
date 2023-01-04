d5ccaddb35a41f782b2be8e9bef744913e5e080f
Kévin <kevin.blaise24@gmail.com>
2023-01-04 12:59:15 +0100

    add form error handling

--- web-app/src/Pages/Profile.jsx
+++ web-app/src/Pages/Profile.jsx
@@ -12,6 +12,7 @@ export default function Profile() {
         headers: { Authorization: process.env.REACT_APP_TOKEN }
     };
 
+    const [formErrorsBag, setFormErrorsBag] = useState(null);
     const [openDangerModal, setOpenDangerModal] = useState(false);
 
     const deleteAccount = () => {
@@ -20,6 +21,7 @@ export default function Profile() {
 
     const submitForm = (e) => {
         e.preventDefault();
+        setFormErrorsBag(null);
         const form = new FormData(e.target);
         const data = {};
         if (!!form.get('firstName')) Object.assign(data, { firstName: form.get('firstName') });
@@ -31,102 +33,63 @@ export default function Profile() {
 
         if (Object.keys(data).length > 0) {
             axios.put(BASE_URL + "/users/edit", data, config)
-            .then((res) => {
-                console.log(res);
-                if (res.status === 200) toast.success('Profile updated !');
-            })
-            .catch((err) => {
-                // TODO: form errors handling
-                console.log(err.response.data)
-            });
+                .then((res) => {
+                    if (res.status === 200) toast.success('Profile updated !');
+                })
+                .catch((err) => {
+                    if(err.response.status === 422) setFormErrorsBag(err.response.data.errors);
+                });
         }
     }
 
     return (
         <>
-            <div className="mt-16 mx-0 sm:mt-0">
+            <div className="mt-16 mx-0 md:mt-0">
                 <form onSubmit={submitForm}>
-                    <div className="overflow-hidden shadow sm:rounded-md">
-                        <div className="bg-white px-4 py-5 sm:p-6">
+                    <div className="overflow-hidden shadow md:rounded-md">
+                        <div className="bg-white px-4 py-5 md:p-6">
                             <div className="grid grid-cols-6 gap-6">
-                                <div className="inline-grid col-span-6 sm:col-span-6 justify-items-center sm:justify-items-start">
+                                <div className="inline-grid col-span-6 md:col-span-6 justify-items-center md:justify-items-start">
                                     <Avatar initials='KB' />
                                 </div>
-                                <div className="col-span-6 sm:col-span-3">
-                                    <label htmlFor="firstName" className="block text-sm font-medium text-gray-700">
-                                        First name
-                                    </label>
-                                    <input
-                                        type="text"
-                                        name="firstName"
-                                        id="firstName"
-                                        autoComplete='given-name'
-                                    />
+                                <div className={`col-span-6 md:col-span-3 ${formErrorsBag?.firstname ? "form-error-div" : ""}`}>
+                                    <label htmlFor="firstName">First name</label>
+                                    <input name="firstName" id="firstName" autoComplete='given-name'/>
+                                    { formErrorsBag?.firstname && <div className="form-error-field">{ formErrorsBag.firstname[0] }</div> }
                                 </div>
 
-                                <div className="col-span-6 sm:col-span-3">
-                                    <label htmlFor="lastName" className="block text-sm font-medium text-gray-700">
-                                        Last name
-                                    </label>
-                                    <input
-                                        type="text"
-                                        name="lastName"
-                                        id="lastName"
-                                        autoComplete='family-name'
-                                    />
+                                <div className={`col-span-6 md:col-span-3 ${formErrorsBag?.lastname ? "form-error-div" : ""}`}>
+                                    <label htmlFor="lastName">Last name</label>
+                                    <input name="lastName" id="lastName" autoComplete='family-name'/>
+                                    { formErrorsBag?.lastname && <div className="form-error-field">{ formErrorsBag.lastname[0] }</div> }
                                 </div>
 
-                                <div className="col-span-6">
-                                    <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                                        Email address
-                                    </label>
-                                    <input
-                                        type="text"
-                                        name="email"
-                                        id="email"
-                                        autoComplete='email'
-                                    />
+                                <div className={`col-span-6 ${formErrorsBag?.email ? "form-error-div" : ""}`}>
+                                    <label htmlFor="email">Email address</label>
+                                    <input name="email" id="email" autoComplete='email'/>
+                                    { formErrorsBag?.email && <div className="form-error-field">{ formErrorsBag.email[0] }</div> }
                                 </div>
 
-                                <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                                    <label htmlFor="old_password" className="block text-sm font-medium text-gray-700">
-                                        Current Password
-                                    </label>
-                                    <input
-                                        type="password"
-                                        name="old_password"
-                                        id="old_password"
-                                        autoComplete='current-password'
-                                    />
+                                <div className={`col-span-6 md:col-span-6 lg:col-span-2 ${formErrorsBag?.old_password ? "form-error-div" : ""}`}>
+                                    <label htmlFor="old_password">Current Password</label>
+                                    <input type="password" name="old_password" id="old_password" autoComplete='current-password'/>
+                                    { formErrorsBag?.old_password && <div className="form-error-field">{ formErrorsBag.old_password[0] }</div> }
                                 </div>
 
-                                <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                                    <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                                        New Password
-                                    </label>
-                                    <input
-                                        type="password"
-                                        name="password"
-                                        id="password"
-                                        autoComplete='new-password'
-                                    />
+                                <div className={`col-span-6 md:col-span-3 lg:col-span-2 ${formErrorsBag?.password ? "form-error-div" : ""}`}>
+                                    <label htmlFor="password">New Password</label>
+                                    <input type="password" name="password" id="password" autoComplete='new-password'/>
+                                    { formErrorsBag?.password && <div className="form-error-field">{ formErrorsBag.password[0] }</div> }
                                 </div>
 
-                                <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                                    <label htmlFor="password_confirmation" className="block text-sm font-medium text-gray-700">
-                                        Confirm New Password
-                                    </label>
-                                    <input
-                                        type="password"
-                                        name="password_confirmation"
-                                        id="password_confirmation"
-                                        autoComplete='new-password'
-                                    />
+                                <div className="col-span-6 md:col-span-3 lg:col-span-2">
+                                    <label htmlFor="password_confirmation">Confirm New Password</label>
+                                    <input type="password" name="password_confirmation" id="password_confirmation" autoComplete='new-password'/>
                                 </div>
                             </div>
                         </div>
 
-                        <div className="bg-gray-50 px-4 py-3 flex justify-between sm:px-6">
+                        <div className="bg-gray-50 px-4 py-3 flex justify-between md:px-6">
                             <Button
                                 type={"warning"}
                                 onClick={(e) => {
